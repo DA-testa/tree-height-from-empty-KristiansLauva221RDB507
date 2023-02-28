c45f81f551c6ee7e262344589f2eaf338d8691f4
KristiansLauva221RDB507 <125013016+KristiansLauva221RDB507@users.noreply.github.com>
2023-02-28 20:43:31 +0200

    Add files via upload

--- tree_height.py
+++ tree_height.py
@@ -1,60 +1,37 @@
 import sys
 import threading
-
+import numpy as np
 
 def compute_height(n, parents):
-    # create a dictionary to store the children of each node
-    children = {i: [] for i in range(n)}
-
-    # find the root node and populate the children dictionary
-    root = -1
-    for i in range(n):
+   
+    paren = numpy.zeros(n)
+    def height(i):
+        if paren[i] != 0:
+            return paren[i]
         if parents[i] == -1:
-            root = i
+            paren[i] == 1
         else:
-            children[parents[i]].append(i)
-
-    # iterative function to find the height of each subtree
-    stack = [(root, 1)]
-    max_height = 1
-    while stack:
-        node, height = stack.pop()
-        max_height = max(max_height, height)
-        for child in children[node]:
-            stack.append((child, height + 1))
-
-    # compute the height of the whole tree
-    return max_height
-
+            paren[i] = height(parents[i]) + 1
+        return int(max(paren))
 
 def main():
-    # read input from stdin or file
-    mode = sys.stdin.readline().strip()
-    if mode == 'F':
-        filename = sys.stdin.readline().strip()
+    
+    mode = input()
+    if "F" in mode:
+        filename = input()
         if "a" not in filename:
-            with open(filename, 'r') as f:
-                n = int(f.readline().strip())
-                parents = list(map(int, f.readline().strip().split()))
-        else:
+            with open(str("test/"+filename), mode="r") as f:
+                nav = int(f.readline().strip())
+                parent = list(map(int, f.readline().split()))
+        else :
             print("error")
-    elif "I" in mode:
-        n = int(sys.stdin.readline().strip())
-        parents = list(map(int, sys.stdin.readline().strip().split()))
+    elif "I"in mode:
+        nav = int(input())
+        parent = list(map(int, input().split()))
     else:
         print("Invalid input mode.")
-        return
-
-    # compute the height of the tree
-    height = compute_height(n, parents)
-
-    # print the result
-    print(height)
-
-
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
+    print(compute_height(nav, parent))
+        
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
